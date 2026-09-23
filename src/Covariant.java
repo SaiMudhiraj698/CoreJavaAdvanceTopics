0db0369ca61517cfc1ac19371648d0d8f33f7459
Sai <saimudiraj698@gmail.com>
2026-09-23 14:02:18 +0530

    Update Covariant example

--- src/Covariant.java
+++ src/Covariant.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+class Animal {
+    Animal giveBirth() {
+        return new Animal();
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    Dog giveBirth() { // Covariant Return Type: 'Dog' is a subtype of 'Animal'
+        return new Dog();
+    }
+
+    void bark() {
+        System.out.println("Woof!");
+    }
+}
+
+////---------------------------------------
+//// BEFORE JAVA 5 (No Covariance)
+//class Animal {
+//    Animal giveBirth() { return new Animal(); }
+//}
+//
+//class Dog extends Animal {
+//    @Override
+//    Animal giveBirth() { return new Dog(); } // Must return "Animal" reference
+//
+//    void bark() { System.out.println("Woof!"); }
+//}
 
 public class Covariant {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        Dog puppy = myDog.giveBirth();
+        puppy.bark(); // Prints: Woof!
+
+//        Dog myDog = new Dog();
+//
+//// The method returns an 'Animal' reference, so you MUST cast it manually to 'Dog'
+//        Dog puppy = (Dog) myDog.giveBirth();
+//        puppy.bark();
+
+    }
 }
